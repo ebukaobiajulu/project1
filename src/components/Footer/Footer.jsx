f7fb75ffd6e5910e58ba198b3486e9cb5127cd4f
Ebuka <ebukano@yahoo.com>
2024-07-09 20:41:54 +0100

    updated changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./footer.css";
 import FooterLogo from "../../assets/footer-logo.png";
+import sendIcon from "../../assets/send-icon.png";
 import FooterImg from "../../assets/footer-frame.png";
 import FacebookIcon from "../../assets/social-media/Icon-Facebook.png";
 import TwitterIcon from "../../assets/social-media/Icon-Twitter.png";
@@ -11,69 +12,71 @@ const Footer = () => {
   return (
     <div className="footer-main">
       <div className="footer">
-          <div className="container">
-            <div className="footer-col">
-              <ul className="footer-menu">
-                <img src={FooterLogo} alt="" className="footer-logo"/>
-                <li>Subscribe</li>
-                <li>Get 10% off your first order</li>
+        <div className="container">
+          <div className="footer-col">
+            <ul className="footer-menu">
+              <img src={FooterLogo} alt="" className="footer-logo" />
+              <li>Subscribe</li>
+              <li>Get 10% off your first order</li>
+              <div className="footer-form-field">
                 <input
                   type="email"
                   className="footer-form"
                   placeholder="Enter your email"
                 />
-              </ul>
-              <ul className="footer-menu">
-                <li className="first-item">Support</li>
-                <li>12, Alhaja Adenike Street, iju road, Lagos </li>
-                <li>ihamroland@gmail.com</li>
-                <li>+234 807 4238929</li>
-              </ul>
-              <ul className="footer-menu">
-                <li className="first-item">Account</li>
-                <li>
-                  <a href="">My Account</a>
-                </li>
-                <li>
-                  <a href="#">Login / Register</a>
-                </li>
-                <li>Cart</li>
-                <li>Shop</li>
-              </ul>
-              <ul className="footer-menu">
-                <li className="first-item">Quick Link</li>
-                <li>Privacy Policy</li>
-                <li>Terms Of Use</li>
-                <li>FAQ</li>
-                <li>Contact</li>
-              </ul>
-              <ul className="footer-menu">
-                <li className="first-item">Download App</li>
-                <li>Save $3 with App New User Only</li>
-                <img src={FooterImg} alt="" />
-                <div className="social-media">
-                  <a href="">
-                    <img src={FacebookIcon} alt="" />
-                  </a>
-                  <a href="">
-                    <img src={TwitterIcon} alt="" />
-                  </a>
-                  <a href="">
-                    <img src={InstagramIcon} alt="" />
-                  </a>
-                  <a href="">
-                    <img src={LinkedinIcon} alt="" />
-                  </a>
-                </div>
-              </ul>
-            </div>
-          
+                <img src={sendIcon} alt="" className="send-icon"/>
+              </div>
+            </ul>
+            <ul className="footer-menu">
+              <li className="first-item">Support</li>
+              <li>12, Alhaja Adenike Street, iju road, Lagos </li>
+              <li>ihamroland@gmail.com</li>
+              <li>+234 807 4238929</li>
+            </ul>
+            <ul className="footer-menu">
+              <li className="first-item">Account</li>
+              <li>
+                <a href="">My Account</a>
+              </li>
+              <li>
+                <a href="#">Login / Register</a>
+              </li>
+              <li>Cart</li>
+              <li>Shop</li>
+            </ul>
+            <ul className="footer-menu">
+              <li className="first-item">Quick Link</li>
+              <li>Privacy Policy</li>
+              <li>Terms Of Use</li>
+              <li>FAQ</li>
+              <li>Contact</li>
+            </ul>
+            <ul className="footer-menu">
+              <li className="first-item">Download App</li>
+              <li>Save $3 with App New User Only</li>
+              <img src={FooterImg} alt="" />
+              <div className="social-media">
+                <a href="">
+                  <img src={FacebookIcon} alt="" />
+                </a>
+                <a href="">
+                  <img src={TwitterIcon} alt="" />
+                </a>
+                <a href="">
+                  <img src={InstagramIcon} alt="" />
+                </a>
+                <a href="">
+                  <img src={LinkedinIcon} alt="" />
+                </a>
+              </div>
+            </ul>
           </div>
+        </div>
       </div>
       <div className="copyright">
         <div className="hr-footer"></div>
-          &copy; Copyright Rimel 2022. All right reserved
-        </div>
+        &copy; Copyright Rimel 2022. All right reserved
+      </div>
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,50 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import mainLogo from "../../assets/logo.png"
-import lang from "../../assets/LANG.png"
-import shopBasket from "../../assets/shopping-basket-01.png"
-import heartLogo from "../../assets/heart.png"
-import avatar from "../../assets/avatar.png"
-import search from "../../assets/search-icon.png"
-import "./header-styles.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import mainLogo from "../../assets/logo.png";
+import lang from "../../assets/LANG.png";
+import shopBasket from "../../assets/shopping-basket-01.png";
+import heartLogo from "../../assets/heart.png";
+import avatar from "../../assets/avatar.png";
+import search from "../../assets/search-icon.png";
+import HamburgerMenu from "../Hamburger/Hamburger";
+import hamburgerMenu from "../../assets/hamburger-menu.png";
+import "./header-styles.css";
 
 const Header = () => {
+  
   return (
     <div className="header">
-        <div className="container">
-            <div className="header-flex">
-              <div className="logo">
-                <Link to={"/"}><img src={mainLogo} alt="" /></Link>
-              </div>
-              <div className="search-bar">
-                <input type="search" name="" id="" className="search-bar-input" placeholder="Search"/>
-                <img src={search} alt="" className="search-icon"/>
-              </div>
-              <div className="header-icons">
-                <div className="lang">
-                  <img src={lang} alt="" />
-                  <Link to={"/mycart"}><img src={shopBasket} alt="" /></Link>
-                  <img src={heartLogo} alt="" />
-                  <img src={avatar} alt="" />
-                </div>
-              </div>
+      <div className="container">
+        <div className="header-flex">
+          <div className="logo">
+            <Link to={"/"}>
+              <img src={mainLogo} alt="" />
+            </Link>
+          </div>
+          <HamburgerMenu />
+          <div className="search-bar">
+            <input
+              type="search"
+              name=""
+              id=""
+              className="search-bar-input"
+              placeholder="Search"
+            />
+            <img src={search} alt="" className="search-icon" />
+          </div>
+          <div className="header-icons">
+            <div className="lang">
+              <img src={lang} alt="" />
+              <Link to={"/mycart"}>
+                <img src={shopBasket} alt="" />
+              </Link>
+              <img src={heartLogo} alt="" class/>
+              <img src={avatar} alt="" />
             </div>
+          </div>
         </div>
-        
+      </div>
     </div>
-  )
-}
-export default Header
+  );
+};
+export default Header;
